c14a23058ba72929606fb132b5af3325d0c6b51a
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 21:54:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,14 @@
 class Triangle
   def kind(side1,side2,side3)
-    if
+    greater_than_zero == true if side1 > 0 && side2 > 0 && side3 > 0
+    side1_sum == true if side1 < side2 + side3
+    side2_sum == true if side2 < side1 + side3
+    side3_sum == true if side3 < side1 + side2
+    
+    if greater_than_zero == true && side1_sum == true && side2_sum == true && side3_sum == true
       :equilateral if side1 == side2 == side3
+      :isosceles if side1 == side2 || side1 == side3 || side2 == side3
+      :scalene if not :isosceles or :equilateral
     else
       begin
         raise TriangleError
