57a68717b16634caeb69c81ecfaf798e74144a78
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 21:46:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,14 @@
 class Triangle
-  def kind
+  def kind(side1,side2,side3)
+    if 
+      :equilateral if side1 == side2 == side3
+    else
+      begin
+        raise TriangleErr
+      rescue PartnerError => error
+        puts error.message
+      end
+    end 
     #decide if the triangle is equilateral, isosceles, or scalene
     #raise an error if there is an error
   end
