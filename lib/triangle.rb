37a141805a2f2e3f30da76e79237f14842d35120
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 21:57:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,17 @@
 class Triangle
-  def kind(side1,side2,side3)
-    greater_than_zero == true if side1 > 0 && side2 > 0 && side3 > 0
-    side1_sum == true if side1 < side2 + side3
-    side2_sum == true if side2 < side1 + side3
-    side3_sum == true if side3 < side1 + side2
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3    
+  end
+  
+  def kind
+    greater_than_zero == true if @side1 > 0 && @side2 > 0 && @side3 > 0
+    side1_sum == true if @side1 < @side2 + @side3
+    side2_sum == true if @side2 < @side1 + @side3
+    side3_sum == true if @side3 < @side1 + @side2
 
     if greater_than_zero == true && side1_sum == true && side2_sum == true && side3_sum == true
       if side1 == side2 && side1 == side3
