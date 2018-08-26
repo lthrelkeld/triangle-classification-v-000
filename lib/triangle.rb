29061ef106f9c12d7d0a9f8e2fa64b2d31122711
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 21:57:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,9 +4,9 @@ class Triangle
   def initialize(side1,side2,side3)
     @side1 = side1
     @side2 = side2
-    @side3 = side3    
+    @side3 = side3
   end
-  
+
   def kind
     greater_than_zero == true if @side1 > 0 && @side2 > 0 && @side3 > 0
     side1_sum == true if @side1 < @side2 + @side3
