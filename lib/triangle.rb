eba3ca4c9aec83c4d1e3a1d4721e2fd43526f436
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 21:59:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ class Triangle
 
   def kind
     @greater_than_zero == true if @side1 > 0 && @side2 > 0 && @side3 > 0
-    side1_sum == true if @side1 < @side2 + @side3
-    side2_sum == true if @side2 < @side1 + @side3
-    side3_sum == true if @side3 < @side1 + @side2
+    @side1_sum == true if @side1 < @side2 + @side3
+    @side2_sum == true if @side2 < @side1 + @side3
+    @side3_sum == true if @side3 < @side1 + @side2
 
-    if @greater_than_zero == true && side1_sum == true && side2_sum == true && side3_sum == true
-      if side1 == side2 && side1 == side3
+    if @greater_than_zero == true && @side1_sum == true && @side2_sum == true && @side3_sum == true
+      if @side1 == @side2 && @side1 == @side3
         :equilateral
-      elsif side1 == side2 || side1 == side3 || side2 == side3
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
         :isosceles
       else
       :scalene
