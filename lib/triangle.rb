8e872fff4dc8f9eca5c9148c46383e421d2c988d
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 21:42:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,9 @@
 class Triangle
-  # write code here
+  def kind
+    #decide if the triangle is equilateral, isosceles, or scalene
+    #raise an error if there is an error
+  end
+
+  class TriangleError < StandardError
+  end
 end
